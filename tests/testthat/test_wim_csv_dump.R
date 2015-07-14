2399caebd898031f2e7443fe3d448524a9b928cf
James E. Marca <james@activimetrics.com>
2015-07-13 22:08:19 -0700

    test passes getting WIM impute file from fs
    if not paired in couchdb, get from filesystem test passes

--- tests/testthat/test_wim_csv_dump.R
+++ tests/testthat/test_wim_csv_dump.R
@@ -34,6 +34,14 @@ test_that(
 
 test_that(
     "will dump the imputed WIM if no pair in couchdb",{
+        wim_site <- 37
+        wim_dir <- 'S'
+        year <- 2012
+        res <- dump.wim.csv(wim_site,wim_dir,year,
+                            wim_path=path,
+                            output_path=output_path,
+                            trackingdb = parts)
+        expect_that(res,equals('./files/csv/wim.37.S.truck.imputed.2012.csv'))
 
     })
 
